9e7fddc9f2cdbeeb6abe085f2cc51359d79cbc66
zciel <zciel_plus@outlook.com>
2019-03-10 01:20:12 +0800

    3.9

---
+++ node/cn/zciel/basePractice/movePractice/chapter01/025.js中的一等公民函数.js
@@ -0,0 +1,42 @@
+// 一种数据类型
+// 参数
+// 返回值
+// 函数太灵活了，无所不能
+// 一般情况下，把函数作为参数的目的就是为了获取函数内部的异步操作结果
+// JavaScript 单线程、事件循环
+
+/*
+function add(x, y) {
+    return x + y;
+}
+
+let add1 = add(10, 20);
+console.log(add1);
+
+//不会等待
+setTimeout(function () {
+    console.log(2);
+    console.log('hello')
+}, 0)
+*/
+
+
+function add(x, y, callback) {
+    console.log(1);
+    setTimeout(function () {
+        var ret = x + y;
+        callback(ret);
+    }, 10000);
+    console.log(11);
+}
+
+add(10, 20, function (ret) {
+    console.log(ret);
+})
+
+// 注意：凡是需要得到一个函数内部异步操作的结果
+//    setTimeout
+//    readFile
+//    writeFile
+//    ajax
+//   这种情况必须通过：回调函数
---
+++ node/cn/zciel/basePractice/movePractice/chapter02/07.crud-express/student.js
@@ -0,0 +1,147 @@
+/**
+ * student.js
+ * 数据操作文件模块
+ * 职责：操作文件中的数据，只处理数据，不关心业务
+ *
+ * 这里才是我们学习 Node 的精华部分：奥义之所在
+ * 封装异步 API
+ */
+
+
+var fs = require('fs')
+
+var dbPath = './db.json'
+
+//获取学生列表
+exports.find = function (callback) {
+    fs.readFile(dbPath, 'utf8', function (err, data) {
+        if (err) {
+            return callback(err)
+        }
+        callback(null, JSON.parse(data).students)
+    })
+}
+
+//根据id获取学生信息对象
+exports.findById = function (id, callback) {
+    fs.readFile(dbPath, 'utf8', function (err, data) {
+        if (err) {
+            return callback(err);
+        }
+        var students = JSON.parse(data).students;
+        var ret = students.find(function (item) {
+            return item.id === parseInt(id)
+        })
+        callback(null, ret)
+    })
+}
+
+//添加保存的学生
+exports.save = function (student, callback) {
+    fs.readFile(dbPath, 'utf8', function (err, data) {
+        if (err) {
+            return callback(err)
+        }
+        var students = JSON.parse(data).students
+
+        //添加id,唯一不重复
+        student.id = students[students.length - 1].id + 1;
+
+        //把用户传递的对象保存到数组中
+        students.push(student)
+
+        //把对象数据转化为字符串
+        var fileData = JSON.stringify({
+            students: students
+        })
+
+        //把字符串保存到文件中
+        fs.writeFile(dbPath, fileData, function (err) {
+            if (err) {
+                //错误就是把错误对象传递给它
+                return callback(err)
+            }
+            //成功就是没有错，所以错误对象是null
+            callback(null)
+        })
+    })
+}
+
+//更新学生
+exports.updateById = function (student, callback) {
+    fs.readFile(dbPath, 'utf8', function (err, data) {
+        if (err) {
+            return callback(err)
+        }
+
+        var students = JSON.parse(data).students
+
+        //注意：这里记得把id统一转为数字类型
+        student.id = parseInt(student.id)
+
+        // 你要修改谁，就需要把谁找出来
+        // EcmaScript 6 中的一个数组方法：find
+        // 需要接收一个函数作为参数
+        // 当某个遍历项符合 item.id === student.id 条件的时候，find 会终止遍历，同时返回遍历项
+        var stu = students.find(function (item) {
+            return item.id == student.id
+        })
+
+        // 这种方式你就写死了，有 100 个难道就写 100 次吗？
+        // stu.name = student.name
+        // stu.age = student.age
+
+        //遍历拷贝对象
+        for (var key in student) {
+            stu[key] = student[key]
+        }
+
+        //把对象数据转换为字符串
+        var fileData = JSON.stringify({
+            students: students
+        })
+
+        //把字符串保存到文件中
+        fs.writeFile(dbPath, fileData, function (err) {
+            if (err) {
+                //错误就把错误对象传给他
+                return callback(err);
+            }
+            //成功
+            callback(null)
+        })
+
+    })
+}
+
+//删除学生
+exports.deleteById = function (id, callback) {
+    fs.readFile(dbPath, 'utf8', function (err, data) {
+        if (err) {
+            return callback(err);
+        }
+        var students = JSON.parse(data).students
+
+        //findIndex方法专门用来根据条件查找元素的下标
+        var deleteId = students.findIndex(function (item) {
+            return item.id === parseId(id)
+        })
+
+        //根据下标从数组中删除对应的学生对象
+        students.splice(deleteId, 1);
+
+        //把对象数组转换为空字符串
+        var fileData = JSON.stringify({
+            students: students
+        })
+
+        //把字符串保存到文件中
+        fs.writeFile(dbPath, fileData, function (err) {
+            if (err) {
+                return callback(err)
+            }
+            // 成功就没错，所以错误对象是 null
+            callback(null)
+        })
+    })
+}
