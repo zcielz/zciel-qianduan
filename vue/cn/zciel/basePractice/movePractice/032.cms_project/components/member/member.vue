5847da33d77fe7b8d33764c14aa1429230567ca2
zcielz <zciel_plus@outlook.com>
2019-03-11 17:19:59 +0800

    3.11

---
+++ vue/cn/zciel/basePractice/movePractice/032.cms_project/components/member/member.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        我是会员页面
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {}
+        }
+    }
+</script>
+<style>
+
+</style>
